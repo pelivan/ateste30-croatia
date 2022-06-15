6cd50cd34cd192c9833e34f306c82c77cd479b5d
Ivan <80061997+pelivan@users.noreply.github.com>
2022-06-15 10:04:44 +0200

    Add Header
    Added Header component into website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Header />
+    </Router>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  HeaderContainer,
+  HeaderLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./HeaderElement";
+
+const Header = () => {
+  return (
+    <>
+      <Nav>
+        <HeaderContainer>
+          <HeaderLogo to="/">Atest E30 Croatia</HeaderLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">O nama</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="gallery">Galerija</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contact">Kontakt</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </HeaderContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Header;
