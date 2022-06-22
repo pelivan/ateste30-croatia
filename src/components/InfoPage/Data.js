2b90c3c3cd87c1c59209d1525dc479621ebb19f8
Ivan <80061997+pelivan@users.noreply.github.com>
2022-06-22 10:43:38 +0200

    Add InfoPage
    Added first info page that will describe business.

---
+++ src/components/InfoPage/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lighttextDesc: true,
+  topLine: "Preradili ste svoje vozilo i više nije legalno?",
+  headline: "Prepustite nam svu brigu oko atestiranja vašeg vozila.",
+  description:
+    "Našim klijentima nudimo savjetovanje i izradu tehničkog elaborata za nezakonski prerađena vozila na području RH. Pomoću navedenoga naši klijenti imati će zakonski atestirani vozila koja je moguće uredno registrirati.",
+  buttonLabel: "Saznaj više",
+  imgStart: false,
+  img: require("../../images/car-1.svg").default,
+  alt: "car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import InfoPage from "../components/InfoPage";
 import Sidebar from "../components/Sidebar";
+import { homeObjOne } from "../components/InfoPage/Data";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,6 +17,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggleOpen} />
       <Header toggle={toggleOpen} />
       <Hero />
+      <InfoPage {...homeObjOne} />
     </>
   );
 };
