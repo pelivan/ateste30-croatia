0131350c24b25a88e97e5f7b130788cf77a04454
Ivan <80061997+pelivan@users.noreply.github.com>
2022-06-23 00:45:21 +0200

    Style update
    Updated header so now it changes color during scrolling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import {
   Nav,
@@ -12,12 +12,24 @@ import {
 import { animateScroll as scroll } from "react-scroll";
 
 const Header = ({ toggle }) => {
+  const [scrollHeader, setScrollHeader] = useState(false);
+  const changeHeader = () => {
+    if (window.scrollY >= 130) {
+      setScrollHeader(true);
+    } else {
+      setScrollHeader(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeHeader);
+  }, []);
   const toggleHome = () => {
     scroll.scrollToTop();
   };
   return (
     <>
-      <Nav>
+      <Nav scrollHeader={scrollHeader}>
         <HeaderContainer>
           <HeaderLogo to="/" onClick={toggleHome}>
             Atest E30 Croatia
