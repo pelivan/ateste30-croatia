c0b7c66ba5df903268e4d553b4130d19374a9641
Ivan Pelivan <ivan.pelivan@codeasy.com>
2025-08-28 15:52:10 +0200

    Remove console log

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,6 @@ import Footer from "../components/Footer";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(isOpen);
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   };
